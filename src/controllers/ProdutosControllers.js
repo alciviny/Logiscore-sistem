c73f0b05fc722fb0451a4aaee60c799594214f5a
alciviny <alcionisviniciusdossantossilva@gmail.com>
2025-11-10 00:33:09 -0300

    feature/adicionado rotas,services e controllers dos produtos

---
+++ src/controllers/ProdutosControllers.js
@@ -0,0 +1,19 @@
+
+ const axios = require('axios')
+ 
+ class Produtos {
+   static async ProdutosGet(req,res){
+
+   }
+   static async ProdutosDelete(req,res){
+
+   }
+   static async ProdutosPut(req,res){
+
+   }
+   static async ProdutosPost(req,res){
+
+   }
+ }
+
+ export default Produtos;
---
+++ src/routes/produtosRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const rota = express.Router()
+
+rota.get('/produtos',)
+rota.patch('/produtos:id',)
+rota.delete('/produtos:id',)
+rota.post('/produtos')
+
+export default rota();
