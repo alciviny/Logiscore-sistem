502f7c139fbc5db12d0118d5164fde712c17f958
rftal rft <rftal1@fiema.org.br>
2025-11-11 20:49:32 -0300

    feature/contollers incrementado na aula senai

--- src/controllers/ProdutosControllers.js
+++ src/controllers/ProdutosControllers.js
@@ -3,7 +3,18 @@ import axios from 'axios'
  
  class Produtos {
    static async ProdutosGet(req,res){
-
+     const response =await fetch(process.env.MONGODB)
+     if(!response){
+       return []
+     }
+     try{
+        res.status(200).json(response)
+     }
+     catch(error){
+      const mensagem = "nao foi possivel  se conectar ao banco de dados"
+      console.log("nao conseguimos puxar os dados do banco de dados ",error)
+      res.status(500).send(mensagem)
+     }
    }
    static async ProdutosDelete(req,res){
 
@@ -12,7 +23,9 @@ import axios from 'axios'
 
    }
    static async ProdutosPost(req,res){
-
+    const produtos = req.body
+    const response = await axios.post(process.env.MONGODB,produtos)
+    .then(response.data)
    }
  }
 
